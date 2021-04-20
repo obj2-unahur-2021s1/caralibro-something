3fbaf4df6bd4c4cf22b0bca54afe543e641f6038
Damian Takashima <d.takas23@gmail.com>
2021-04-20 19:34:45 -0300

    hasta permisos

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -3,7 +3,15 @@ package ar.edu.unahur.obj2.caralibro
 import kotlin.math.ceil
 
 abstract class Publicacion {
+  var cantidadMeGusta = 0
+  var usuariosMeGusta = mutableListOf<Usuario>()
+//  var permiso =
+
   abstract fun espacioQueOcupa(): Int
+
+  fun meGusta() = cantidadMeGusta ++
+
+  fun agregarUsuario(usuario: Usuario) = usuariosMeGusta.add(usuario)
 }
 
 class Foto(val alto: Int, val ancho: Int) : Publicacion() {
@@ -13,4 +21,23 @@ class Foto(val alto: Int, val ancho: Int) : Publicacion() {
 
 class Texto(val contenido: String) : Publicacion() {
   override fun espacioQueOcupa() = contenido.length
+}
+
+class Video(val duracion: Int, var calidad: String ) : Publicacion() {
+//  faltaria una comprobacion o usar enums
+//  val calidades = listOf<String>("SD", "HD720p", "HD1080p")
+
+  override fun espacioQueOcupa(): Int {
+    var peso = duracion
+    if (calidad == "HD720p") {
+      peso *= 3
+    } else if (calidad == "HD1080p") {
+      peso *= 6
+    }
+    return peso
+  }
+
+  fun cambiarCalidad(calidadNueva: String) {
+    calidad = calidadNueva
+  }
 }
--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -2,10 +2,21 @@ package ar.edu.unahur.obj2.caralibro
 
 class Usuario {
   val publicaciones = mutableListOf<Publicacion>()
+  val publicacionesMeGustan = mutableListOf<Publicacion>()
 
   fun agregarPublicacion(publicacion: Publicacion) {
     publicaciones.add(publicacion)
   }
 
   fun espacioDePublicaciones() = publicaciones.sumBy { it.espacioQueOcupa() }
+
+// revisar
+  fun darMeGusta(publicacion: Publicacion) {
+    if (!publicacionesMeGustan.contains(publicacion)) {
+      publicacion.meGusta()
+      publicacion.agregarUsuario(this)
+      publicacionesMeGustan.add(publicacion)
+    }
+  }
 }
+
